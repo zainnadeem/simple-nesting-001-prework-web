cb262af67351b25769e3d8d2344ffe6cc26d34e6
Deniz Unat <deniznida@users.noreply.github.com>
2015-08-13 13:55:51 -0400

    Update nested.rb

--- nested.rb
+++ nested.rb
@@ -107,7 +107,7 @@ def changing_alan
      #change what Alan Kay is :known_for the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      programmer_hash
 end
 
